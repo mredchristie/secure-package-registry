5e0f8f77b1ac6ac78861b4d9709e580947a005db
Benjamin Read <readb5@cardiff.ac.uk>
2026-03-21 03:55:23 +0000

    Some quick port fixes

--- home-ui/svelte.config.js
+++ home-ui/svelte.config.js
@@ -7,6 +7,10 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter(),
+		prerender: {
+			// For now, since the navbar includes links to pages that don't exist yet, we need to disable crawling until we add those pages.
+			crawl: false,
+		},
 	},
 };
 
